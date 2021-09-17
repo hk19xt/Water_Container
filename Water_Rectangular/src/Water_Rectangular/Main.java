106b42278615687df9c1e159ae2984afe2fd3753
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-09-17 15:57:46 -0400

    Add files via upload

---
+++ Water_Rectangular/src/Water_Rectangular/Main.java
@@ -0,0 +1,7 @@
+package Water_Rectangular;
+
+public class Main {
+    public static void main(String[] args){
+
+    }
+}//Main
---
+++ Water_Rectangular/src/Water_Rectangular/Water_Rectangular.java
@@ -0,0 +1,7 @@
+package Water_Rectangular;
+
+public class Water_Rectangular {
+    public int Rectangular (int[] userValues){
+
+    }
+}//Water_Rectangular
