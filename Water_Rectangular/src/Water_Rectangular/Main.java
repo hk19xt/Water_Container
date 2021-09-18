cd47fd21df765b7d729c78dcd684cbb19be6240a
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-09-18 15:44:56 -0400

    Update Main.java

--- Water_Rectangular/src/Water_Rectangular/Main.java
+++ Water_Rectangular/src/Water_Rectangular/Main.java
@@ -1,7 +1,21 @@
 package Water_Rectangular;
 
+/* This is the main class. 
+   Users can change the input values in the hardcoded line.
+   This program is to find the maximum rectangular area by calculating the first height multiplied by the second height.
+   Author: Hyejin Kim
+   Date: 9/18/2021
+*/
 public class Main {
+    
     public static void main(String[] args){
-
+        
+        int[] userValues = {8,3,2,6,4,5,1,8,2}; //Users can change the input values here.
+      
+        int output = 0;
+        
+        Water_Rectangular WR = new Water_Rectangular();
+        output = WR.Rectangular(userValues);
+        System.out.println("The maximum contationer will be :" + output);
     }
 }//Main
